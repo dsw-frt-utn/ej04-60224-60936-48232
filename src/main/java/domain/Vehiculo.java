56880d70d112f7761c15ac55970b4bd07305b7ce
Franco704 <franmartinez704@gmail.com>
2026-04-14 20:01:06 -0300

    Agregado clase Marcas y sus cambios involucrados

--- src/main/java/domain/Vehiculo.java
+++ src/main/java/domain/Vehiculo.java
@@ -2,14 +2,14 @@ package domain;
 
 public abstract class Vehiculo {
     protected String patente;
-    protected String marca;
+    protected Marca marca;
     protected String modelo;
     protected int anio;
     protected double capacidadCarga;
     protected Sucursal sucursal;
     private VehiculoTipo tipo;
 
-    public Vehiculo(VehiculoTipo tipo, String patente, String marca, String modelo, int anio, double capacidadCarga, Sucursal sucursal) {
+    public Vehiculo(VehiculoTipo tipo, String patente, Marca marca, String modelo, int anio, double capacidadCarga, Sucursal sucursal) {
         this.patente = patente;
         this.marca = marca;
         this.modelo = modelo;
@@ -39,15 +39,13 @@ public abstract class Vehiculo {
         return sucursal.getCodigo();
     }
     
-    public double calcularConsumo(double kilometros) {
-        return 0;
-    }
+    public abstract double calcularConsumo(double kilometros);
 
     public boolean esDe(VehiculoTipo tipo){
         return this.tipo == tipo;
     }
     @Override
     public String toString() {
-        return patente + " - " + marca + " " + modelo + " - Sucursal: " + sucursal.getCodigo();
+        return marca.toString() + " " + modelo + " - Sucursal: " + sucursal.getCodigo();
     }
 }
